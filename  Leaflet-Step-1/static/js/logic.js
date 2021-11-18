f0cebfe6216805b44d12e57d56402a582c35f967
annaweeks <aoweeks@yahoo.com>
2021-11-18 02:01:36 -0800

    added color to markers and legend

---  Leaflet-Step-1/static/js/logic.js
+++  Leaflet-Step-1/static/js/logic.js
@@ -1,4 +1,14 @@
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+// function selects a color for the earthquake depth
+function getColor(d) {
+    return d >= 90 ? '#800026' :
+           d >= 70  ? '#bd0026' :
+           d >= 50  ? '#e31a1c' :
+           d > 30  ? '#fc4e2a' :
+           d > 10   ? '#fd8d3c' :
+           '#ffeda0' ;
+           
+}
 
 function createMap(earthquakes) {
 
@@ -15,7 +25,7 @@ function createMap(earthquakes) {
     // create an overlayMaps object
     var overlayMaps = {
         "Earthquakes": earthquakes
-      };
+    };
     
       // create the map object
       var map = L.map('map', {
@@ -26,8 +36,28 @@ function createMap(earthquakes) {
     
     // create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
     L.control.layers(baseMap, overlayMaps, {
-        collapse: false
+        collapse: false,
     }).addTo(map);
+
+     //LEGEND____________________________________________
+     var legend = L.control({position: 'bottomright'});
+
+     legend.onAdd = function () {
+ 
+         var div = L.DomUtil.create('div', 'info legend'),
+             grades = [-10, 10, 30, 50, 70, 90],
+             labels = [];
+ 
+         // loop through our density intervals and generate a label with a colored square for each interval
+         for (var i = 0; i < grades.length; i++) {
+             div.innerHTML +=
+                 '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+                 grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+         }
+         return div;
+     };
+ 
+     legend.addTo(map);
 }
 
 function createMarkers(response) {
@@ -35,19 +65,25 @@ function createMarkers(response) {
     // pull the earthquakes from the data
     var features = response.features;
 
-    var earthquakeMarkers = []
+    var earthquakeMarkers = [];
 
     // loop through earthquakes and make array of markers
     for (var i = 0; i < features.length; i++) {
 
         var earthquake = features[i];
+        var magnitude = earthquake.properties.mag;
+        var depthColor = getColor(earthquake.geometry.coordinates[2]);
 
-        var earthquakeMarker = L.marker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]])
-            .bindPopup("<h3>URL: " + earthquake.properties.url + 
+        var earthquakeMarker = L.circle([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], {
+            radius: magnitude * 10000,
+            color:  depthColor
+        }).bindPopup("<h3>URL: " + earthquake.properties.url + 
             "</h3><h3>Time: " + earthquake.properties.time + "</h3>" +
             "</h3><h3>Title: " + earthquake.properties.title + "</h3>" +
-            "</h3><h3>Place: " + earthquake.properties.place + "</h3>" 
-            );
+            "</h3><h3>Place: " + earthquake.properties.place + "</h3>" +
+            "</h3><h3>Magnitude: " + earthquake.properties.mag + "</h3>" +
+            "</h3><h3>Depth: " + earthquake.geometry.coordinates[2] + "</h3>" 
+        );
 
         earthquakeMarkers.push(earthquakeMarker);
     }
